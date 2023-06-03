58ffc8e6a9ca227a26b771880d62412176816dba
nalendrooo <nalendroadil274@gmail.com>
2023-06-03 18:48:06 +0700

    test ci cd all done

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const dbConnect = async () => {
 dbConnect()
 
 const app = express()
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 9000
 
 app.use(cookieParser())
 app.use(cors({ credentials: true, origin: '*' }))
